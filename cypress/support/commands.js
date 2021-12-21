8158cd34c33eb0ca720e71e264d59d5fffa1e34a
Kristin Jackvony <kristin.jackvony@nextsteplivinginc.com>
2021-12-21 12:37:51 -0500

    Replacing double quotes with single quotes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,8 +3,8 @@ Cypress.Commands.add('loginAPI', () => {
             method: 'POST',
             url: 'http://thinking-tester-contact-list.herokuapp.com/users/login',
             body: {
-                "email": "testguilduser@fake.com",
-                "password": "foobarfoo"
+                'email': 'testguilduser@fake.com',
+                'password': 'foobarfoo'
             }
         }).then((response) => {
             Cypress.env('token', response.body.token)
@@ -34,17 +34,17 @@ Cypress.Commands.add('addContactAPI', (firstName) => {
         url: 'http://thinking-tester-contact-list.herokuapp.com/contacts',
         headers: {'Authorization': `Bearer ${Cypress.env('token')}` },
         body: {
-            "firstName": firstName,
-            "lastName": "Prunewhip",
-            "birthdate": "1969-05-13",
-            "email": "pprunewhip@fake.com",
-            "phone": "8008675309",
-            "street1": "123 Sesame St.",
-            "street2": "Apt. A",
-            "city": "New York",
-            "stateProvince": "NY",
-            "postalCode": "01234",
-            "country": "USA"
+            'firstName': firstName,
+            'lastName': 'Prunewhip',
+            'birthdate': '1969-05-13',
+            'email': 'pprunewhip@fake.com',
+            'phone': '8008675309',
+            'street1': '123 Sesame St.',
+            'street2': 'Apt. A',
+            'city': 'New York',
+            'stateProvince': 'NY',
+            'postalCode': '01234',
+            'country': 'USA'
             }
     }).then((response) => {
         Cypress.env('id', response.body._id)
